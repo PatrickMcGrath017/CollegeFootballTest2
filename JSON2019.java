8216151b3e8650cf081081fa138cf78d9721d659
Patrick McGrath <pmcgrat3@student04.cse.nd.edu>
2020-09-24 00:11:23 -0400

    9-24 Functionality Additions

--- JSON2019.java
+++ JSON2019.java
@@ -7,17 +7,21 @@ class Game {
 
 	// class definition
 	int week;
+	String venue;
 	String homename;
 	int homescore;
 	String awayname;
 	int awayscore;
+	float excitementindex;
 
-	Game(int week, String homename, int homescore, String awayname, int awayscore){
+	Game(int week, String venue, String homename, int homescore, String awayname, int awayscore, float excitementindex){
 		this.week = week;
+		this.venue = venue;
 		this.homename = homename;
 		this.homescore = homescore;
 		this.awayname = awayname;
 		this.awayscore = awayscore;
+		this.excitementindex = excitementindex;
 	} // end of Game definition
 	
 	// setters & getters 
@@ -26,6 +30,12 @@ class Game {
 	}
 
 	int getWeek() { return this.week; }
+
+	void setVenue(String venue){
+		this.venue = venue;
+	}
+
+	String getVenue() { return this.venue; }
 
 	void setHomeName(String homename){
 		this.homename = homename;
@@ -50,6 +60,12 @@ class Game {
 	}
 
 	int getAwayScore() { return this.awayscore; }
+
+	void setExcitementIndex(float excitementindex){
+		this.excitementindex = excitementindex;
+	}
+
+	float getExcitementIndex() { return this.excitementindex; }
 	// end of seters & getters
 
 	public String toString(){
@@ -78,6 +94,8 @@ public class JSON2019{
 				//week
 				Long weekLong = (Long) jo.get("week");
 				int week = weekLong.intValue();
+				//venue
+				String venue = (String) jo.get("venue");
 				//homename
 				String homename = (String) jo.get("home_team");
 				//homescore
@@ -88,8 +106,17 @@ public class JSON2019{
 				//awayscore
 				Long awayscoreLong = (Long) jo.get("away_points");
 				int awayscore = awayscoreLong.intValue();
+				//excitementindex
+				String excitementindexString = (String) jo.get("excitement_index");
+				float excitementindex;
+				if(excitementindexString == null){
+					excitementindex = 0.0f;
+				}
+				else{
+					excitementindex = Float.parseFloat(excitementindexString);
+				}
 				// add to games list
-				games[i] = new Game(week, homename, homescore, awayname, awayscore);
+				games[i] = new Game(week, venue, homename, homescore, awayname, awayscore, excitementindex);
 				i++;
 			}
 
@@ -104,7 +131,13 @@ public class JSON2019{
 
 				Game[] games = parseJsonGames(ja);
 
-				String getInput = "Avaliable actions:\n printall\t Prints all games.\n printTeam\t Prints all games of a team.\n printWeek\t Prints all games of a week.\n printTeamWeek\t Prints all games of a team in a week.\n exit";
+				String getInput = 	"Avaliable actions:\n " +
+									"printall\t Prints all games.\n " +
+									"printTeam\t Prints all games of a team.\n " +
+									"printWeek\t Prints all games of a week.\n " +
+									"printTeamWeek\t Prints all games of a team in a week.\n " +
+									"printDetailedGame\t Prints specific details of a single game.\n " +
+									"exit";
 
 				System.out.println(getInput);
 
@@ -158,6 +191,29 @@ public class JSON2019{
 						}
 						if(playedthisweek == false){
 							System.out.println(team + " did not play this week");
+						}
+					}
+					else if(option.compareTo("printDetailedGame") == 0){
+						System.out.println("Enter home team:");
+						String hometeam = newScan.nextLine();
+						System.out.println("Enter away team:");
+						String awayteam = newScan.nextLine();
+						boolean playedthisgame = false;
+						for(Game game : games){
+							if(hometeam.compareTo(game.homename) == 0 && awayteam.compareTo(game.awayname) == 0){
+								playedthisgame = true;
+								if(game.homescore > game.awayscore){
+									System.out.println("In week #" + game.week + " " + game.homename + " beat " + 
+													game.awayname + " by the score of " + game.homescore + " to " + game.awayscore + " at " + game.venue);
+								}
+								else{
+									System.out.println("Int week #" + game.week + " " + game.awayname + " beat " + 
+													game.homename +" by the score of " + game.awayscore + " to " + game.homescore + " at " + game.venue);
+								}
+							}
+						}
+						if(playedthisgame == false){
+							System.out.println(awayteam + " did not play at " + hometeam);
 						}
 					}
 					else if(option.compareTo("exit") == 0){
